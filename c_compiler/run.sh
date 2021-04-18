6a837b31e47a38f56b93f5ce247455ad4bb53877
Alysson <4ly.alcantara@gmail.com>
2021-04-18 19:04:39 -0300

    Update run.sh

--- c_compiler/run.sh
+++ c_compiler/run.sh
@@ -1,6 +1,11 @@
-src=https://raw.githubusercontent.com/4ly-a/shellscript_library/master/c_compiler/c_compiler.sh
+src=https://raw.githubusercontent.com/4ly-a/shellscript_library/master/c_compiler/c_compiler.sh 
 
 wget -q ${src}
 
 . ./c_compiler.sh
 
+. ./delete_shell.sh
+
+delete && rm delete_shell.sh 
+
+clear
